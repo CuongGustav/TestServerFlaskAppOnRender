4537302c1e9b0a59e8b14cacb6a548a48d8b6ced
CuongGustav <cuonggustav@gmail.com>
2024-10-31 00:11:53 +0700

    modify main.py and add render.yaml

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 import base64
+import os
 import io
 import cv2
 from PIL import Image
@@ -83,5 +84,8 @@ def upload_image():
         'vietnamese_text': result_vietnamese
     })
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ biến môi trường, mặc định là 5000
+    app.run(host='0.0.0.0', port=port)
