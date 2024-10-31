41531de18b1c41ddf17b5e788a2680c5926973e8
CuongGustav <cuonggustav@gmail.com>
2024-10-31 07:37:33 +0700

    update

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ def upload_image():
         'vietnamese_text': result_vietnamese
     })
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ biến môi trường, mặc định là 5000
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ biến môi trường, mặc định là 5000
+#     app.run(host='0.0.0.0', port=port)
